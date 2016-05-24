abb12e52e0d6838c816ee881ce0288a2208b7249
choiben314 <choiben314@gmail.com>
2016-05-24 12:29:00 -0400

    Plan for grille cipher project.

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// number grid 1-16 in each quadrant
+		// generate key with 1-16
+		// get each line from message and fill end of each line with ^'s
+		// make line backwards
+		// put each character in line into one of 16 spaces in key (left to right for each row)
+		// rotate key 90 degrees clockwise and fill in 16 more spaces. Keep rotating until 64 spaces are filled (one line).
+		// copy each 1*8 row of 8*8 grid = coded group.
+		// this is the coded message!
+
+	}
+
+}
