6978b7211c21c5cc3e2cd62fcaa012ba8e21a84c
choiben314 <choiben314@gmail.com>
2016-05-24 12:53:43 -0400

    Preliminary IO methods.

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
+	public static Scanner fileReader;
+	public static Scanner userIn = new Scanner(System.in);
+	public static String fileName = "default.txt";
+	public static String encryptFile = "encrypt.txt";
+	public static PrintWriter pw;
+	
 	public static void main(String[] args) {
-		
+		 
 		// number grid 1-16 in each quadrant
 		// generate key with 1-16
 		// get each line from message and fill end of each line with ^'s
@@ -12,6 +20,32 @@ public class Main {
 		// copy each 1*8 row of 8*8 grid = coded group.
 		// this is the coded message!
 
+		System.out.println("Enter file name with text.");
+		fileName = userIn.next();
+		
+		initScanner(fileName);
+		initOutput(encryptFile);
+		
+	}
+	public static void initOutput(String fileName) {
+		File file = new File(fileName);
+		pw = null;
+		try {
+			pw = new PrintWriter(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("Cannot create " + fileName);
+			System.exit(1);
+		}
+	}
+	
+	public static void initScanner(String fileName) {
+		File file = new File(fileName);
+		try {
+			fileReader = new Scanner(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found at \"" + fileName + "\".");
+			ex.printStackTrace();
+		}
 	}
 
 }
